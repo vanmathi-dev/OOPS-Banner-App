d3162e257be65c60a370d6c8062c65f6fc57800e
vanmathi-dev <vanmathi.karunanithi@bridgelabz.com>
2026-01-29 17:56:19 +0530

    store character pattern in a class

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,34 +1,31 @@
+class CharacterPattern{
+    private char character;
+    private String[] pattern;
+
+    public CharacterPattern(char character, String[] pattern){
+        this.character = character;
+        this.pattern = pattern;
+    }
+    public String getLine(int index){
+        return pattern[index];
+    }
+    public char getCharacter(){
+        return character;
+    }
+}
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] o = getOPattern();
-        String[] p = getPPattern();
-        String[] s = getSPattern();
-
-        String[] banner = new String[o.length];
-
-        for (int i = 0; i<o.length; i++){
-            banner[i] =o[i] +" "+o[i] + " "+ p[i] +" "+s[i];
-        }
-
-        for (String line: banner){
-            System.out.println(line);
-        }
-    }
-    //Method to generate O pattern
-    private static String[] getOPattern(){
-        return new String[]{
+        CharacterPattern o = new CharacterPattern('O', new String[]{
                 "    ***    ",
                 " **     ** ",
                 "**       **",
                 "**       **",
                 "**       **",
                 " **     ** ",
-                "    ***    ",
-        };
-    }
-    //Method to generate P pattern
-    private static String[] getPPattern(){
-        return new String[]{
+                "    ***    "
+        });
+
+        CharacterPattern p = new CharacterPattern('P', new String[]{
                 "******  ",
                 "**    **",
                 "**    **",
@@ -36,10 +33,9 @@ public class OOPSBannerApp {
                 "**      ",
                 "**      ",
                 "**      "
-        };
-    }
-    private static String[] getSPattern(){
-        return new String[]{
+        });
+
+        CharacterPattern s = new CharacterPattern('S',new String[]{
                 "    *****  ",
                 "  **       ",
                 " **        ",
@@ -47,6 +43,17 @@ public class OOPSBannerApp {
                 "      **   ",
                 "     **    ",
                 "*****     "
-        };
+        });
+
+        CharacterPattern[] word ={o,o,p,s};
+        for (int i=0;i<7;i++){
+            StringBuilder line = new StringBuilder();
+
+            for(CharacterPattern cp: word){
+                line.append(cp.getLine(i)).append(" ");
+            }
+            System.out.println(line);
+        }
     }
+
 }
