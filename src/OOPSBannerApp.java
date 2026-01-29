3b0b15f7951dc3908e7e578aa9a3fcca7127b7de
vanmathi-dev <vanmathi.karunanithi@bridgelabz.com>
2026-01-29 17:43:37 +0530

    UC6-RefactorBannerIntoMethods : refractor banner logic into functions

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,17 +1,52 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] lines = {
-                String.join("", "    ***    ", "    ***    "," ", "******  ","    *****  "),
-                String.join(""," **     ** "," **     ** "," ", "**    **", "  **       "),
-                String.join("", "**       **","**       **"," ", "**    **"," **        "),
-                String.join("", "**       **","**       **"," ",  "******  ",  "   ***     "),
-                String.join("", "**       **","**       **"," ", "**      ", "      **   "),
-                String.join("", " **     ** "," **     ** "," ", "**      ", "     **    "),
-                String.join("",  "    ***    ",  "    ***    "," ","**      ", "*****     ")
-        };
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-        for (String line : lines){
+        String[] banner = new String[o.length];
+
+        for (int i = 0; i<o.length; i++){
+            banner[i] =o[i] +" "+o[i] + " "+ p[i] +" "+s[i];
+        }
+
+        for (String line: banner){
             System.out.println(line);
         }
+    }
+    //Method to generate O pattern
+    private static String[] getOPattern(){
+        return new String[]{
+                "    ***    ",
+                " **     ** ",
+                "**       **",
+                "**       **",
+                "**       **",
+                " **     ** ",
+                "    ***    ",
+        };
+    }
+    //Method to generate P pattern
+    private static String[] getPPattern(){
+        return new String[]{
+                "******  ",
+                "**    **",
+                "**    **",
+                "******  ",
+                "**      ",
+                "**      ",
+                "**      "
+        };
+    }
+    private static String[] getSPattern(){
+        return new String[]{
+                "    *****  ",
+                "  **       ",
+                " **        ",
+                "   ***     ",
+                "      **   ",
+                "     **    ",
+                "*****     "
+        };
     }
 }
